533ed117d8042ead67ae60e87205f182dfe0ec44
Reesejones1989 <mauricejonesjr@gmail.com>
2023-09-28 13:53:44 -0400

    New Complete

---
+++ server.js
@@ -0,0 +1,17 @@
+//INDUCES
+
+const express = require('express')
+const router = express.Router();
+
+
+//INDEX
+//NEW
+
+router.get('/new', (req, res) => {
+res.render('New')})
+
+//DELETE
+//UPDATE
+//CREATE
+//EDIT
+//Show
---
+++ views/New.jsx
@@ -0,0 +1,19 @@
+const react = require('react')
+
+class New extends React.Component {
+    render(){
+        return (
+            <div>
+
+                <form action="/logs" method="POST">
+                    <input type='text' name='title'/><br/>
+                    <input type='textarea' name='entry'/><br/>
+                    <input type='checkbox' name='shipIsBroken'/><br/>
+                    <input type="submit" />
+
+                </form>
+
+            </div>
+        )
+    }
+}
